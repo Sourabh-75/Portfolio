c2d806deb164c5e6e083183021d21eefbf7dfa19
Sourav Singh <sourav859987@gmail.com>
2024-07-19 23:58:33 +0530

    hover on design that design open in new window

--- thumbnailPage/design.js
+++ thumbnailPage/design.js
@@ -10,3 +10,16 @@ galleryItems.forEach((item) => {
     item.classList.remove('hover');
   });
 });
+
+// hover on design
+
+let designImages = document.querySelectorAll('.yt-thumbnail');
+
+designImages.forEach((image) => {
+  image.addEventListener('click', (e) => {
+    // image source
+    let imgSrc = image.querySelector('img').src;
+    // Open the image in a new window
+    window.open(imgSrc, '_blank');
+  });
+});
