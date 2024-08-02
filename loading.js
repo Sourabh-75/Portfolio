775e4a6bb79b71ad2d2ebedcf20d8ad3cb661ceb
Sourav Singh <sourav859987@gmail.com>
2024-08-02 10:45:08 +0530

    add loading image css file

---
+++ loading.js
@@ -0,0 +1,29 @@
+// function images loading one by one
+const imagesArray = document.querySelectorAll('.loading-image img');
+
+let images = Array.from(imagesArray)
+
+function loadImagesSequentially() {
+  let index = 0;
+
+  function loadNextImage() {
+    if (index < images.length) {
+      const image = images[index];
+      image.src = image.dataset.src;
+      index++;
+
+      globalThis.setTimeout(loadNextImage, 100);
+    }
+  }
+
+  // Start loading the first image
+  loadNextImage();
+}
+
+images.forEach((image) => {
+  const src = image.src;
+  image.src = '';
+  image.dataset.src = src;
+});
+
+loadImagesSequentially();
