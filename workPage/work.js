a49c8a30de3d1c121ad2030c1e728cb3c5c036c1
Sourav Singh <sourav859987@gmail.com>
2024-08-02 12:26:36 +0530

    remove extra code and make optimize design

---
+++ workPage/work.js
@@ -0,0 +1,27 @@
+// add event listener to gallery items
+const galleryItems = document.querySelectorAll('.gallery-item');
+
+galleryItems.forEach((item) => {
+  item.addEventListener('mouseover', () => {
+    item.classList.add('hover');
+  });
+
+  item.addEventListener('mouseout', () => {
+    item.classList.remove('hover');
+  });
+});
+
+// hover on design
+
+let designImages = document.querySelectorAll('.gallery-item');
+
+let images = Array.from(designImages)
+
+images.forEach((image) => {
+  image.addEventListener('click', (e) => {
+    // image source
+    let imgSrc = image.querySelector('img').src;
+    // Open the image in a new window
+    window.open(imgSrc, '_blank');
+  });
+});
